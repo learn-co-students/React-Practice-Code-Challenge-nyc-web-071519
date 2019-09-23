a8801dced230043f60a270935633e5b513555d75
thenatural86 <philip.smalls86@gmail.com>
2019-09-23 14:06:09 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,20 +1,66 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react"
+import SushiContainer from "./containers/SushiContainer"
+import Table from "./containers/Table"
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = { sushiList: [], sushiCount: 0 }
+
+  componentDidMount() {
+    fetch(`${API}`)
+      .then(resp => resp.json())
+      .then(data => {
+        let sushiArray = data.map(datum => {
+          return { ...datum, eaten: false }
+        })
+        this.setState({ sushiList: sushiArray })
+      })
+  }
+
+  sushiLimiter = () => {
+    let sushiArray = this.state.sushiList.slice(
+      this.state.sushiCount,
+      this.state.sushiCount + 4
+    )
+    return sushiArray
+  }
+
+  eatSushiHandler = sushiObj => {
+    let newArray = this.state.sushiList.map(sushi => {
+      if (sushi.id === sushiObj.id) {
+        sushiObj.eaten = true
+        return sushi
+      } else {
+        return sushi
+      }
+    })
+    this.setState({ sushiList: newArray })
+  }
+
+  moreSushiHandler = () => {
+    // console.log("more sushi")
+    this.setState({ sushiCount: this.state.sushiCount + 4 })
+  }
+
+  plateHandler = () => {
+    return this.state.sushiList.filter(sushi => sushi.eaten)
+  }
 
   render() {
+    // console.log(this.state.sushiArray)
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer
+          sushiList={this.sushiLimiter()}
+          eatSushiHandler={this.eatSushiHandler}
+          moreSushiHandler={this.moreSushiHandler}
+        />
+        <Table sushiList={this.plateHandler()} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,23 @@
-import React, { Fragment } from 'react'
-import MoreButton from '../components/MoreButton'
+import React, { Fragment } from "react"
+import MoreButton from "../components/MoreButton"
+import Sushi from "../components/Sushi"
 
-const SushiContainer = (props) => {
+const SushiContainer = props => {
+  // console.log(props)
+  let sushi = props.sushiList.map(sushiObj => {
+    return (
+      <Sushi
+        key={sushiObj.id}
+        sushi={sushiObj}
+        eatSushiHandler={props.eatSushiHandler}
+      />
+    )
+  })
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {sushi}
+        <MoreButton moreSushiHandler={props.moreSushiHandler} />
       </div>
     </Fragment>
   )
